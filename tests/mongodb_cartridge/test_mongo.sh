abe326c9b0b6ba771d29a4dafb001819fdf7e1db
vagrant <filirom1@gmail.com>
2013-04-30 09:55:36 +0000

    add support for mongodb

---
+++ tests/mongodb_cartridge/test_mongo.sh
@@ -0,0 +1,21 @@
+LOGIN=`rhc app show mongodbapp    |grep Username | awk '{print $NF}'`
+PASSWORD=`rhc app show mongodbapp |grep Password | awk '{print $NF}'`
+
+rhc port-forward mongodbapp &
+
+# http://stackoverflow.com/questions/360201/kill-background-process-when-shell-script-exit
+trap "kill 0" SIGINT SIGTERM EXIT
+sleep 5
+
+PORT=`rhc-list-ports 2>&1 |grep "270" | head -n1 | cut -d: -f2`
+
+expect << EOF
+  spawn mongo --host localhost --port $PORT admin
+  expect ">"
+  send -- "db.auth(\"$LOGIN\", \"$PASSWORD\")\r"
+  expect "1"
+  send -- "show dbs\r"
+  expect "mongodbapp"
+  send -- "quit()\r"
+  expect eof
+EOF
